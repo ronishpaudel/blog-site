23aea6e20a8c82fb510dd4292f316eaf28363ae4
robuGdada <paudelronish@gmail.com>
2023-08-03 22:26:51 +0545

    add axios interceptors

--- src/api/API.ts
+++ src/api/API.ts
@@ -3,3 +3,14 @@ import axios from "axios";
 export const API = axios.create({
   baseURL: "http://localhost:3001",
 });
+
+API.interceptors.request.use(
+  (axiosConfig) => {
+    const token = localStorage.getItem("jwtToken");
+    if (token && axiosConfig.headers) {
+      axiosConfig.headers["Authorization"] = `${token}`;
+    }
+    return axiosConfig;
+  },
+  (error) => Promise.reject(error)
+);
