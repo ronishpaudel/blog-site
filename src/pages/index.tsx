646d78bf6d00188fc60253fe0d402cfe322b97c5
robuGdada <paudelronish@gmail.com>
2023-08-07 22:44:54 +0545

    chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
-import { Main } from "@/components/Main";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Main from "@/components/Main";
 
 const index = () => {
   return (
