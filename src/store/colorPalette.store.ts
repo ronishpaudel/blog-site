20cf3803ce08b368b520be28a926c156d287e066
robuGdada <paudelronish@gmail.com>
2023-08-07 12:34:37 +0545

    color store

---
+++ src/store/colorPalette.store.ts
@@ -0,0 +1,20 @@
+import { proxy } from "valtio";
+
+export const COLOR_PALETTE = {
+  black: "#000000",
+  white: "#FFFFFF",
+};
+
+export type ColorName = keyof typeof COLOR_PALETTE;
+
+interface ColorPalette {
+  color: ColorName;
+  setColor: (color: ColorName) => void;
+}
+
+export const colorPaletteStore = proxy<ColorPalette>({
+  color: "white",
+  setColor(color) {
+    colorPaletteStore.color = color;
+  },
+});
