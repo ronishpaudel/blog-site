7fa714d0ff62054f3f070c8122c838c62ead3ce1
robuGdada <paudelronish@gmail.com>
2023-08-10 19:19:07 +0545

    chore

---
+++ public/images/icons/index.ts
@@ -0,0 +1,14 @@
+export { default as Arrowclockwise } from "./Arrowclockwise.svg";
+export { default as Arrowcounterclockwise } from "./Arrowcounterclockwise.svg";
+export { default as Textparagraph } from "./Textparagraph.svg";
+export { default as Chevrondown } from "./Chevrondown.svg";
+export { default as Typebold } from "./Typebold.svg";
+export { default as Typeitalic } from "./Typeitalic.svg";
+export { default as Typeunderline } from "./Typeunderline.svg";
+export { default as Typestrikethrough } from "./type-strikethrough.svg";
+export { default as Code } from "./code.svg";
+export { default as TypeLink } from "./link.svg";
+export { default as TextLeft } from "./text-left.svg";
+export { default as TextCenter } from "./text-center.svg";
+export { default as TextRight } from "./text-right.svg";
+export { default as TextJustify } from "./justify.svg";
