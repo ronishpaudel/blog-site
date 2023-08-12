ac468cbb25a5984e2dc1e130ac4cfe07b9ea1b08
robuGdada <paudelronish@gmail.com>
2023-08-12 13:22:07 +0545

    wrap by lexical editor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,17 +16,64 @@ import "./create-blog/index.css";
 import "../components/lexical/editor.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Work_Sans } from "@next/font/google";
+import { useEffect } from "react";
+import { authStore } from "@/store/authStore";
+import {
+  getItemFromLocalStorage,
+  removeItemFromLocalStorage,
+} from "@/store/storage";
+import { useSnapshot } from "valtio";
+import { API } from "@/api/API";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { editorConfig } from "@/components/lexical/Editor";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
 });
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+  const { dbUser } = useSnapshot(authStore);
+
+  const token =
+    typeof window !== "undefined" ? getItemFromLocalStorage("auth") : "";
+
+  const fetchDBUser = async () => {
+    console.log("hello user");
+    const res = await API.get("/user/me");
+    return res.data;
+  };
+
+  const fetchUser = async () => {
+    const data = await fetchDBUser();
+    if (data) {
+      authStore.setDbUser(data);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      authStore.setLoggedIn();
+      authStore.setTokenFetching(false);
+      try {
+        if (!dbUser?.id && token) {
+          fetchUser();
+        }
+      } catch (error) {
+        authStore.setLogOut();
+        removeItemFromLocalStorage("auth");
+      }
+    } else {
+      authStore.setLogOut();
+      authStore.setTokenFetching(false);
+    }
+  }, [token]);
 
   return (
     <div className={workSans.className}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <LexicalComposer initialConfig={editorConfig}>
+          <Component {...pageProps} />
+        </LexicalComposer>
       </QueryClientProvider>
     </div>
   );
