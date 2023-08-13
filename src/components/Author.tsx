24270536ddc842de9242a0973c543bcf5dd55d96
robuGdada <paudelronish@gmail.com>
2023-08-14 00:02:11 +0545

    ausing actual author name

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,10 +1,15 @@
+import { useAuthorInfo } from "@/hooks/useAuthorInfo";
 import React from "react";
 
 const Author = () => {
+  const { data: fetchUserInfo } = useAuthorInfo();
   return (
     <div className="author">
       <img src="/rbg.jpg" className="author-img" />
-      <p className="ronish">Ronish Paudel</p>
+      <p className="ronish">
+        {fetchUserInfo?.fname} {fetchUserInfo?.lname}
+      </p>
+
       <p>August 20,2023</p>
     </div>
   );
