ffdb3373078ca39f16f15bd9df8e282feac3b472
robuGdada <paudelronish@gmail.com>
2023-08-14 00:04:44 +0545

    not working now

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,12 +1,29 @@
+import { useCategoryQuery } from "@/hooks/useGetCategory";
+import { useOneBlog } from "@/hooks/useQueryBlog";
+import { useRouter } from "next/router";
 import React from "react";
 
-const Tag = ({ style }: any) => {
+interface TagProps {
+  style?: React.CSSProperties;
+  category?: string;
+}
+
+const Tag = ({ style }: TagProps) => {
+  const { data: categoryData } = useCategoryQuery();
+  const { query } = useRouter();
+  const { data: blogData } = useOneBlog(query?.id as string);
+
+  const findCategoryName = (categoryId: string) => {
+    const category = categoryData?.find(
+      (category) => category.id === Number(categoryId)
+    );
+    return category ? category.name : "Unknown Category";
+  };
+
   return (
-    <>
-      <p style={style} className="category">
-        Technology
-      </p>
-    </>
+    <p style={style} className="category">
+      {blogData && <span>{findCategoryName(blogData.categoryId)}</span>}
+    </p>
   );
 };
 
