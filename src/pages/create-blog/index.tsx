74a6a2f3fc446adc15e151560b1ee65283b566ca
robuGdada <paudelronish@gmail.com>
2023-08-13 16:54:40 +0545

    add image upload and mutate title des

--- src/pages/create-blog/index.tsx
+++ src/pages/create-blog/index.tsx
@@ -1,9 +1,5 @@
 import { TextInput } from "@/components/TextInput";
-import { zodResolver } from "@hookform/resolvers/zod";
-import React, { FC, useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { useCreateBlog } from "./useCreateBlog";
+import React, { ChangeEvent, FC, useState } from "react";
 import { useRouter } from "next/router";
 import Button from "@/components/Button";
 import { PrivateRoute } from "@/components/hoc/PrivateRoute";
@@ -11,39 +7,68 @@ import Editor from "@/components/lexical/Editor";
 import Header from "@/components/Header";
 import { $generateHtmlFromNodes } from "@lexical/html";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import parse from "html-react-parser";
 import { proxy } from "valtio";
+import { useCategoryQuery } from "@/hooks/useGetCategory";
 
 export const blogCreationStore = proxy<{
   title: string;
   description: string;
-  setDetail: (title: string, description: string) => void;
+  categoryId: string;
+  imageUrl: string;
+  setDetail: (title: string, description: string, category: string) => void;
+  setImage: (imageUrl: string) => void;
 }>({
   title: "",
   description: "",
-  setDetail(title, description) {
+  categoryId: "",
+  imageUrl: "",
+  setDetail(title, description, categoryId) {
     this.title = title;
     this.description = description;
+    this.categoryId = categoryId;
+  },
+  setImage(imageUrl) {
+    this.imageUrl = imageUrl;
   },
 });
 
 const index: FC = () => {
   const [title, setTitle] = useState("");
   const { push } = useRouter();
-  const [htmlToParse, setHtmlToParse] = useState("");
+  const [file, setFile] = useState<any>(null);
+  const [fileType, setFileType] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const { data } = useCategoryQuery();
   const [editor] = useLexicalComposerContext();
 
   function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setTitle(e.target.value);
   }
 
+  const handleImageSelect = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        setFile(e?.target?.result as any);
+      };
+      reader.readAsDataURL(file);
+      setFileType(file.type);
+    }
+  };
+
+  const removeImage = () => {
+    setFile(null);
+    setFileType("");
+  };
+
   return (
     <>
       <Header />
-      {htmlToParse && parse(htmlToParse)}
-      {/* {console.log(htmlToParse && parse(htmlToParse))} */}
+      {/* {htmlToParse && parse(htmlToParse)} */}
       <div className="form-container">
-        <div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
           <div className="title-input">
             <h3 style={{ paddingBottom: "20px" }}>Title</h3>
             <TextInput
@@ -54,6 +79,53 @@ const index: FC = () => {
               onChange={handleTitleChange}
               maxWidth="mW700"
             />
+            <div className="error-message">
+              {!title ? <div>Title required</div> : ""}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div className="uploadImg">
+              <input type="file" onChange={handleImageSelect} />
+              {file && (
+                <div className="selected-image-container">
+                  <img src={file} alt="Selected Image" />
+                  <p
+                    style={{
+                      position: "relative",
+                      right: "0.5%",
+                      cursor: "pointer",
+                      fontWeight: "630",
+                    }}
+                    onClick={removeImage}
+                  >
+                    x
+                  </p>
+                </div>
+              )}
+            </div>
+            <div>
+              <select
+                className="categoryy"
+                value={categoryId}
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  setCategoryId(e.target.value);
+                }}
+              >
+                {data &&
+                  data.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
           </div>
           <div>
             <h3>Description</h3>
@@ -68,12 +140,14 @@ const index: FC = () => {
           >
             <Button
               type="submit"
-              text={"Publish"}
+              text={"preview your blog"}
               onClick={() => {
                 editor.update(async () => {
                   const htmlString = $generateHtmlFromNodes(editor, null);
-                  blogCreationStore.setDetail(title, htmlString);
+                  blogCreationStore.setDetail(title, htmlString, categoryId);
+                  blogCreationStore.setImage(file);
                 });
+                console.log({ file: blogCreationStore.setImage(file) });
                 push("/recheck-blog");
               }}
             />
@@ -85,4 +159,3 @@ const index: FC = () => {
 };
 
 export default PrivateRoute(index);
-// export default index;
--- src/pages/recheck-blog/index.tsx
+++ src/pages/recheck-blog/index.tsx
@@ -4,33 +4,124 @@ import { blogCreationStore } from "../create-blog";
 import parse from "html-react-parser";
 import { useCreateBlog } from "../create-blog/useCreateBlog";
 import { useRouter } from "next/router";
+import Footer from "@/components/Footer";
+import { Author } from "@/components/Author";
+import { $generateHtmlFromNodes } from "@lexical/html";
+import Header from "@/components/Header";
+import Button from "@/components/Button";
+import { useState } from "react";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useUploadUrl } from "@/hooks/useImageUploadUrl";
+import { fileToBlob } from "@/utils/filetoBlob";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { useCategoryQuery } from "@/hooks/useGetCategory";
 
+type UploadResponse = {
+  message: string;
+  data: { uploadUrl: string; url: string };
+};
 function index() {
-  const { title, description } = useSnapshot(blogCreationStore);
+  const { title, description, imageUrl, categoryId } =
+    useSnapshot(blogCreationStore);
 
-  const router = useRouter();
+  const { push } = useRouter();
+  const [file, setFile] = useState<any>(null);
+  const [fileType, setFileType] = useState("");
+  const [editor] = useLexicalComposerContext();
+  const { data: categoryName } = useCategoryQuery();
 
-  const { mutateAsync: createBlog } = useCreateBlog({
-    onSuccess: async (data) => {
-      console.log("onSuccess triggered", data);
+  const selectedCategory = categoryName?.find(
+    (category) => category.id === categoryId
+  );
+
+  console.log("Category ID:", categoryId);
+  console.log("Category Name Array:", categoryName);
+  console.log({ imageUrl });
+  const { mutateAsync: createBlog } = useCreateBlog();
+
+  const uploadToS3 = async (val: { uploadUrl: string; blobData: Blob }) => {
+    return await axios({
+      method: "put",
+      url: val.uploadUrl,
+      headers: {
+        "Content-Type": fileType,
+      },
+      data: val.blobData,
+    });
+  };
+  const { mutateAsync: s3Mutate } = useMutation({
+    mutationFn: uploadToS3,
+  });
+
+  const { mutate: createBlogWithImage } = useUploadUrl({
+    onSuccess: async (res: UploadResponse) => {
+      const file = imageUrl;
+      console.log({ file });
+      if (file) {
+        console.log({ file });
+        editor.update(async () => {
+          const htmlString = $generateHtmlFromNodes(editor, null);
+          const blobData = await fileToBlob(file, fileType);
+          console.log({ dd: res.data.uploadUrl });
+          await s3Mutate({ uploadUrl: res.data.uploadUrl, blobData: blobData });
+          await createBlog({
+            title,
+            description: htmlString,
+            imageUrl: res.data.url,
+            categoryId: Number(categoryId),
+          });
+        });
+
+        push("/");
+      } else {
+        console.log("else ma janu vo sir");
+        editor.update(async () => {
+          const htmlString = $generateHtmlFromNodes(editor, null);
+          await createBlog({
+            title,
+            description: htmlString,
+            categoryId: Number(categoryId),
+          });
+        });
+        push("/");
+      }
+    },
+    onError: (error: any) => {
+      console.log(error);
     },
   });
 
+  const handleOnSubmit = () => {
+    createBlogWithImage({});
+  };
+
   return (
     <div>
-      <h1>{title}</h1>
-      <div>{description && parse(description)}</div>
-      <button
-        onClick={async () => {
-          await createBlog({
-            title: title,
-            description: description,
-          });
-          // router.push("/");
-        }}
-      >
-        save my blog
-      </button>
+      <Header />
+      <div className="page-wrapper">
+        <div className="blog-info">
+          <div>preview of your blog</div>
+          <p className="category" key={categoryId}>
+            {selectedCategory ? selectedCategory.name : "Unknown Category"}
+          </p>
+          <h1>{title}</h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+          >
+            <Author />
+          </div>
+        </div>
+        <img src={imageUrl} className="blog-image" />
+        <div style={{ width: "100%" }}>{description && parse(description)}</div>
+        <Button text={"publish now"} onClick={() => handleOnSubmit()} />
+      </div>
+      <Footer />
     </div>
   );
 }
