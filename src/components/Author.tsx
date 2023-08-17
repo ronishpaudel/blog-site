195a1cdb7090362df67f8599540b0a4fb9b7e3b3
robuGdada <paudelronish@gmail.com>
2023-08-17 14:29:44 +0545

    help find user acording to userid

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,16 +1,29 @@
 import { useAuthorInfo } from "@/hooks/useAuthorInfo";
+import { useOneBlog } from "@/hooks/useQueryBlog";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Author = () => {
-  const { data: fetchUserInfo } = useAuthorInfo();
+  const { data: userData } = useAuthorInfo();
+  const { query } = useRouter();
+  const { data: blogData } = useOneBlog(query?.id as string);
+
+  const findUserName = (userId: number) => {
+    const user = userData?.find((user) => user.id === Number(userId));
+    return user ? `${user.fname} ${user.lname}` : "Unknown Author";
+  };
+
   return (
     <div className="author">
-      <img src="/rbg.jpg" className="author-img" />
+      <img src="/rbg.jpg" className="author-img" alt="Author" />
       <p className="ronish">
-        {fetchUserInfo?.fname} {fetchUserInfo?.lname}
+        {blogData ? (
+          <span>{findUserName(blogData.userId)}</span>
+        ) : (
+          "Loading author..."
+        )}
       </p>
-
-      <p>August 20,2023</p>
+      <p>{"August 20, 2023"}</p>
     </div>
   );
 };
