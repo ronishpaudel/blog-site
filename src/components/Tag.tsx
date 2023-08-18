a343be653b630368ecca29389a696d269cc7aeff
robuGdada <paudelronish@gmail.com>
2023-08-18 12:46:00 +0545

    passing as pprops

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,28 +1,14 @@
-import { useCategoryQuery } from "@/hooks/useGetCategory";
-import { useOneBlog } from "@/hooks/useQueryBlog";
-import { useRouter } from "next/router";
 import React from "react";
 
 interface TagProps {
   style?: React.CSSProperties;
-  category?: string;
+  category: { id?: number; name: string };
 }
 
-const Tag = ({ style }: TagProps) => {
-  const { data: categoryData } = useCategoryQuery();
-  const { query } = useRouter();
-  const { data: blogData } = useOneBlog(query?.id as string);
-
-  const findCategoryName = (categoryId: string) => {
-    const category = categoryData?.find(
-      (category) => category.id === Number(categoryId)
-    );
-    return category ? category.name : "";
-  };
-
+const Tag = ({ style, category }: TagProps) => {
   return (
     <p style={style} className="category">
-      {blogData && <span>{findCategoryName(blogData.categoryId)}</span>}
+      {category?.name}
     </p>
   );
 };
