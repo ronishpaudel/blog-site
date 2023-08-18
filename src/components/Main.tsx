f3ad9b672c6ce7865eb7ae184c311498af80981a
robuGdada <paudelronish@gmail.com>
2023-08-18 20:34:25 +0545

    chre

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,15 +4,44 @@ import Card from "./Card";
 import Ad from "./Ad";
 import { useRouter } from "next/router";
 import { Iblog, useQueryBlog } from "@/hooks/useQueryBlog";
+import { dateFormat } from "@/utils/dateFormat";
 
-const Main: FC = () => {
-  const { data, hasNextPage, fetchNextPage } = useQueryBlog();
+const GG = () => {
   const { push } = useRouter();
 
+  const { data, hasNextPage, fetchNextPage } = useQueryBlog();
+
+  return (
+    <div className="card-parent" id="main">
+      {data?.pages.map((page: any) =>
+        page.map((blog: Iblog) => (
+          <Card
+            key={blog.id}
+            category={blog?.category?.name}
+            title={blog.title}
+            image={blog.imageUrl}
+            user={`${blog?.user?.fname} ${blog?.user?.lname}`}
+            createdAt={dateFormat(blog.createdAt)}
+            onCardClick={() => {
+              push({
+                pathname: "/[id]",
+                query: {
+                  id: blog.id,
+                },
+              });
+            }}
+            onProfileClick={() => console.log("navigate to profile with id")}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+const Main: FC = () => {
   const handleViewNextPost = async () => {
-    if (hasNextPage) {
-      await fetchNextPage();
-    }
+    // if (hasNextPage) {
+    //   await fetchNextPage();
+    // }
   };
 
   return (
@@ -24,31 +53,7 @@ const Main: FC = () => {
           </div>
           <Content />
           <Ad />
-          <div className="card-parent" id="main">
-            {data?.pages.map((page: any) =>
-              page.map((blog: Iblog) => (
-                <Card
-                  key={blog.id}
-                  category={blog?.category?.name}
-                  title={blog.title}
-                  image={blog.imageUrl}
-                  user={`${blog?.user?.fname} ${blog?.user?.lname}`}
-                  onCardClick={() => {
-                    push({
-                      pathname: "/[id]",
-                      query: {
-                        id: blog.id,
-                      },
-                    });
-                  }}
-                  onProfileClick={() =>
-                    console.log("navigate to profile with id")
-                  }
-                  createdAt={""}
-                />
-              ))
-            )}
-          </div>
+          <GG />
           <div className="viewPost" onClick={handleViewNextPost}>
             View All Post
           </div>
