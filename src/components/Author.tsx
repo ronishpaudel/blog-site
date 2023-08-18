1fc453ff4cc0bdd522e739f6f038f95289abbaf2
robuGdada <paudelronish@gmail.com>
2023-08-18 12:46:09 +0545

    pass as props

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -1,29 +1,24 @@
-import { useAuthorInfo } from "@/hooks/useAuthorInfo";
-import { useOneBlog } from "@/hooks/useQueryBlog";
-import { useRouter } from "next/router";
 import React from "react";
 
-const Author = () => {
-  const { data: userData } = useAuthorInfo();
-  const { query } = useRouter();
-  const { data: blogData } = useOneBlog(query?.id as string);
-
-  const findUserName = (userId: number) => {
-    const user = userData?.find((user) => user.id === Number(userId));
-    return user ? `${user.fname} ${user.lname}` : "Unknown Author";
+interface IUserInfo {
+  style?: React.CSSProperties;
+  user: {
+    fname: string;
+    lname: string;
   };
+  createdAt: string;
+}
 
+const Author = ({ style, user, createdAt }: IUserInfo) => {
   return (
     <div className="author">
       <img src="/rbg.jpg" className="author-img" alt="Author" />
-      <p className="ronish">
-        {blogData ? (
-          <span>{findUserName(blogData.userId)}</span>
-        ) : (
-          "Loading author..."
-        )}
+      <p className="ronish" style={style}>
+        {user?.fname}
+
+        {user?.lname}
       </p>
-      <p>{"August 20, 2023"}</p>
+      <p>{createdAt}</p>
     </div>
   );
 };
