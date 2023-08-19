800601e3e356bc9f60a50cd6ddc8d1bc2aca78cc
robuGdada <paudelronish@gmail.com>
2023-08-19 11:49:18 +0545

    thumbImageUrl add

--- src/store/blogCreationStore.ts
+++ src/store/blogCreationStore.ts
@@ -5,16 +5,19 @@ export const blogCreationStore = proxy<{
   description: string;
   category: { id: number; displayName: string };
   imageUrl: string;
+  thumbImageUrl: string;
   setDescription: (description: string) => void;
   setTitle: (title: string) => void;
   setImage: (imageUrl: string) => void;
   setCategory: (val: { id: number; displayName: string }) => void;
+  setThumbImageUrl: (thumbImageUrl: string) => void;
   clearStore: () => void;
 }>({
   title: "",
   description: "",
   category: { id: 0, displayName: "" },
   imageUrl: "",
+  thumbImageUrl: "",
   setTitle(title) {
     this.title = title;
   },
@@ -26,6 +29,9 @@ export const blogCreationStore = proxy<{
   },
   setCategory(val) {
     this.category = val;
+  },
+  setThumbImageUrl(val) {
+    this.thumbImageUrl = val;
   },
   clearStore() {
     this.title = "";
