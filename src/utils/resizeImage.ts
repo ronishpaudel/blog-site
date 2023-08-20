6ddb5d691c8ff6e359c8ba14eac712644c421947
robuGdada <paudelronish@gmail.com>
2023-08-20 20:32:24 +0545

    resize of image add

--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -1,24 +1,45 @@
-function resizeImage(inputImage, newWidth, newHeight, callback) {
-  const img = new Image();
+// function resizeImage(inputImage, newWidth, newHeight, callback) {
+//   const img = new Image();
 
-  img.onload = function () {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+//   img.onload = function () {
+//     const canvas = document.createElement("canvas");
+//     const ctx = canvas.getContext("2d");
 
+//     canvas.width = newWidth;
+//     canvas.height = newHeight;
+
+//     ctx?.drawImage(img, 0, 0, newWidth, newHeight);
+
+//     const resizedDataURL = canvas.toDataURL("image/jpeg");
+
+//     if (callback) {
+//       callback(resizedDataURL);
+//     }
+//   };
+
+//   img.src = inputImage;
+//   return img.src;
+// }
+
+function resizeImage2(imagePath, newWidth) {
+  const originalImage = new Image();
+  originalImage.src = imagePath;
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas?.getContext("2d");
+  originalImage.addEventListener("load", function () {
+    const originalWidth = originalImage.naturalWidth;
+    const originalHeight = originalImage.naturalHeight;
+    const aspectRatio = originalWidth / originalHeight;
+    const newHeight = newWidth / aspectRatio;
     canvas.width = newWidth;
     canvas.height = newHeight;
 
-    ctx?.drawImage(img, 0, 0, newWidth, newHeight);
-
-    const resizedDataURL = canvas.toDataURL("image/jpeg");
-
-    if (callback) {
-      callback(resizedDataURL);
-    }
-  };
-
-  img.src = inputImage;
-  return img.src;
+    ctx.drawImage(originalImage, 0, 0, newWidth, newHeight);
+    // downloadImage(downloadName);
+    console.log({
+      hellsdo: document.getElementById("canvas").toDataURL("image/jpeg", 0.9),
+    });
+  });
 }
 
-export { resizeImage };
+export { resizeImage2 };
