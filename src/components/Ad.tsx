64343ac7cdbb9cd03c23750563d8d5425b4879f3
robuGdada <paudelronish@gmail.com>
2023-08-24 23:08:04 +0545

    chore

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -4,7 +4,9 @@ const Ad = ({ style }: any) => {
   return (
     <>
       <div style={style} className="AD">
-        <div className="ADs">You can place ads</div>
+        <div className="ADs" style={{ color: "#696A75" }}>
+          You can place ads
+        </div>
       </div>
     </>
   );
