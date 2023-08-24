937d695858976eab22349cc623861c582ef52ed8
robuGdada <paudelronish@gmail.com>
2023-08-24 18:30:23 +0545

    css added and svg added

--- public/index.ts
+++ public/index.ts
@@ -2,3 +2,4 @@ export { default as Vector } from "./Vector.svg";
 export { default as SrLogo } from "./SrLogo.svg";
 export { default as DropDown } from "./DropDown.svg";
 export { default as Logo } from "./Logo.svg";
+export { default as Meta } from "./metaLogo.svg";
