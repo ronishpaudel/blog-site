50b69dc5ae74cda19c5dcceb81edb7e3378eadca
robuGdada <paudelronish@gmail.com>
2023-08-28 17:11:36 +0545

    port change of api

--- src/api/API.ts
+++ src/api/API.ts
@@ -6,7 +6,7 @@ import {
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://localhost:3005",
 });
 
 // Request interceptor
