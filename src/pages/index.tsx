e873d9459acf0f264d1a71dddd85cffcac7fc927
robuGdada <paudelronish@gmail.com>
2023-08-30 23:37:56 +0545

    chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Main from "@/components/Main";
+import { SignUp } from "@/components/signUp";
+import { SignIn } from "@/components/signIn";
 
 const index = () => {
   return (
@@ -9,6 +11,8 @@ const index = () => {
       <Header />
       <Main />
       <Footer />
+      <SignUp />
+      <SignIn />
     </>
   );
 };
