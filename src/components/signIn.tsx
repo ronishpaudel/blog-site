9dffa1960ea698a91d0ecc837c59bd08e8ce67a2
robuGdada <paudelronish@gmail.com>
2023-08-30 23:40:40 +0545

    sigin /signup diaalog

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -17,16 +17,17 @@ import { FcGoogle } from "react-icons/fc";
 import { AiFillGithub } from "react-icons/ai";
 import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
 import { useSnapshot } from "valtio";
+import { modalStore } from "@/store/modalStore";
 
-function SignIn({ onClick }: { onClick: () => void }) {
+function SignIn({ onClick }: { onClick?: () => void }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showSignUp, setShowSignUp] = useState(false);
 
   const { mutate } = useSignInMutation({
     onSuccess: async (res: { token: string }) => {
       saveItemToLocalStorage("auth", res.token);
       authStore.setLoggedIn();
+      window.location.reload();
     },
   });
 
@@ -39,123 +40,112 @@ function SignIn({ onClick }: { onClick: () => void }) {
   }
 
   function handleShowSignUp() {
-    setShowSignUp(true);
+    modalStore.signUpModal.setOpen(true);
+    console.log({ signInModal });
   }
+
   const themeSnap = useSnapshot(themeStore);
+  const { signInModal } = useSnapshot(modalStore);
 
   return (
     <div>
-      {showSignUp ? (
-        <SignUp />
-      ) : (
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button
-              className="max-w-md w-full text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer"
-              onClick={onClick}
-            >
-              login
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <div className="flex items-center justify-between  ">
-                <div>
-                  <h1
-                    className="text-black text-xl"
-                    style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                  >
-                    Welcome to Lorem
-                  </h1>
-                  <h1
-                    className="text-5xl"
-                    style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                  >
-                    Sign in
-                  </h1>
-                </div>
-                <div>
-                  <p className="w-24 text-gray-500">No Account ? </p>
-                  <p
-                    className="text-blue-500 cursor-pointer hover:text-blue-600"
-                    onClick={handleShowSignUp}
-                  >
-                    Sign up
-                  </p>
-                  {showSignUp && <SignUp />}
+      <SignUp />
+      <Dialog open={signInModal.open}>
+        <DialogContent>
+          <DialogHeader>
+            <div className="flex items-center justify-between  ">
+              <div>
+                <h1
+                  className="text-black text-xl"
+                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+                >
+                  Welcome to Lorem
+                </h1>
+                <h1
+                  className="text-5xl"
+                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+                >
+                  Sign in
+                </h1>
+              </div>
+              <div>
+                <p className="w-24 text-gray-500">No Account ? </p>
+                <div
+                  className="text-blue-500 cursor-pointer hover:text-blue-600"
+                  onClick={handleShowSignUp}
+                >
+                  Sign up
                 </div>
               </div>
-              <DialogTitle>
-                <span
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  Enter your username or email address
-                </span>
-                <Input
-                  className="max-w-md mt-2.5 border-gray-400 h-12"
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  style={{ color: THEME_PALETTE[themeSnap.theme].inputBg }}
-                />
-              </DialogTitle>
-              <DialogDescription>
-                <span
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  Enter your Password
-                </span>
-                <Input
-                  className="max-w-md mt-1 border-gray-400 h-12"
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <span className="text-base max-w-md mt-1.5 text-blue-500 cursor-pointer hover:text-blue-600 flex justify-end ">
-                  Forgot password?
-                </span>
-                <div className="mt-10 mb-10">
-                  <Button
-                    variant={"blue"}
-                    onClick={() =>
-                      mutate({
-                        email,
-                        password,
-                      })
-                    }
-                  >
-                    Sign in
-                  </Button>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span
-                    className="bg-background px-2 text-muted-foreground"
-                    style={{
-                      color: THEME_PALETTE[themeSnap.theme].textColor,
-                      backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg,
-                    }}
-                  >
-                    Or continue with
-                  </span>
-                </div>
+            </div>
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-10"
+            >
+              Enter your username or email address
+            </span>
+            <Input
+              className="max-w-md mt-2.5 border-gray-400 h-12"
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmailChange}
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+            />
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-8"
+            >
+              Enter your Password
+            </span>
+            <Input
+              className="max-w-md mt-1 border-gray-400 h-12"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <span className="text-base max-w-md mt-1.5 text-blue-500 cursor-pointer hover:text-blue-600 flex justify-end ">
+              Forgot password?
+            </span>
+            <div className="mt-10 mb-10">
+              <Button
+                variant={"blue"}
+                onClick={() =>
+                  mutate({
+                    email,
+                    password,
+                  })
+                }
+              >
+                Sign in
+              </Button>
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span
+                className="bg-background px-2 text-muted-foreground"
+                style={{
+                  color: THEME_PALETTE[themeSnap.theme].textColor,
+                  backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg,
+                }}
+              >
+                Or continue with
+              </span>
+            </div>
 
-                <div className="grid grid-cols-2 gap-4 m-3">
-                  <Button>
-                    <FcGoogle className="mr-2 h-4 w-4" />
-                    Google
-                  </Button>
-                  <Button>
-                    <AiFillGithub className="mr-2 h-4 w-4" />
-                    Github
-                  </Button>
-                </div>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
+            <div className="w-full grid grid-cols-2 gap-4 mt-2">
+              <Button variant={"outline"}>
+                <FcGoogle className="mr-2 h-4 w-4" />
+                Google
+              </Button>
+              <Button variant={"outline"}>
+                <AiFillGithub className="mr-2 h-4 w-4" />
+                Github
+              </Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- src/components/signUp.tsx
+++ src/components/signUp.tsx
@@ -1,11 +1,4 @@
-import {
-  DialogHeader,
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog";
+import { Dialog, DialogContent } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useState } from "react";
@@ -14,6 +7,7 @@ import { saveItemToLocalStorage } from "@/store/storage";
 import { authStore } from "@/store/authStore";
 import { useSnapshot } from "valtio";
 import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
+import { modalStore } from "@/store/modalStore";
 
 function SignUp({ onSignInClick }: { onSignInClick?: () => void }) {
   const [email, setEmail] = useState("");
@@ -35,97 +29,99 @@ function SignUp({ onSignInClick }: { onSignInClick?: () => void }) {
     onSuccess: async (res: { token: string }) => {
       saveItemToLocalStorage("auth", res.token);
       authStore.setLoggedIn();
+      window.location.reload();
     },
   });
+  function handleOnClick() {
+    modalStore.signUpModal.setOpen(false);
+    console.log({ signUpModal });
+  }
   const themeSnap = useSnapshot(themeStore);
+  const { signUpModal } = useSnapshot(modalStore);
+
   return (
     <div>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant={"blue"}>Sign up</Button>
-        </DialogTrigger>
+      <Dialog open={signUpModal.open}>
         <DialogContent>
-          <DialogHeader>
-            <div className="flex items-center justify-between  ">
-              <div>
-                <h1
-                  className="text-black text-xl"
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  Welcome to Lorem
-                </h1>
-                <h1
-                  className="text-5xl"
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  Sign up
-                </h1>
-              </div>
-              <div>
-                <p className=" text-gray-500">Have an Account ?</p>
-                <p className="text-blue-500 cursor-pointer hover:text-blue-600">
-                  Sign in
-                </p>
-              </div>
+          <div className="flex items-center justify-between  ">
+            <div>
+              <h1
+                className="text-black text-xl"
+                style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              >
+                Welcome to Lorem
+              </h1>
+              <h1
+                className="text-5xl"
+                style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              >
+                Sign up
+              </h1>
             </div>
-            <DialogTitle>
-              <div className="flex flex-col gap-2">
-                <span
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  Enter your email address
-                </span>
-                <Input
-                  className="max-w-md mt-2.5 border-gray-400 h-12"
-                  name="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                />
+            <div>
+              <p className=" text-gray-500">Have an Account ?</p>
+              <p
+                className="text-blue-500 cursor-pointer hover:text-blue-600"
+                onClick={handleOnClick}
+              >
+                Sign in
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-10"
+            >
+              Enter your email address
+            </span>
+            <Input
+              className="max-w-sm border-gray-400 h-12"
+              name="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
 
-                <span
-                  className="mt-4"
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                >
-                  user name{" "}
-                </span>
-                <Input
-                  className="max-w-md mt-1 border-gray-400 h-12"
-                  name="username"
-                  value={username}
-                  onChange={handleUserNameChange}
-                />
-              </div>
-            </DialogTitle>
-            <DialogDescription>
-              <span style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}>
-                Enter your Password
-              </span>
-              <Input
-                className="max-w-md mt-1 border-gray-400 h-12"
-                type="password"
-                name="password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <span className="text-base max-w-md mt-1.5 text-blue-500 cursor-pointer hover:text-blue-600 flex justify-end ">
-                Forgot password?
-              </span>
-              <div className="mt-5 mb-5">
-                <Button
-                  onClick={() =>
-                    mutate({
-                      email,
-                      username,
-                      password,
-                    })
-                  }
-                  className="max-w-md w-full text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer"
-                >
-                  Sign Up
-                </Button>
-              </div>
-            </DialogDescription>
-          </DialogHeader>
+            <span
+              className="mt-2"
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+            >
+              user name
+            </span>
+            <Input
+              className="max-w-sm  border-gray-400 h-12"
+              name="username"
+              value={username}
+              onChange={handleUserNameChange}
+            />
+          </div>
+          <span style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}>
+            Enter your Password
+          </span>
+          <Input
+            className="max-w-sm border-gray-400 h-12"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <span className="text-base max-w-md mt-1.5 text-blue-500 cursor-pointer hover:text-blue-600 flex justify-end ">
+            Forgot password?
+          </span>
+          <div className="mt-5 mb-5">
+            <Button
+              onClick={() =>
+                mutate({
+                  email,
+                  username,
+                  password,
+                })
+              }
+              className="max-w-sm w-full text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer"
+            >
+              Sign Up
+            </Button>
+          </div>
         </DialogContent>
       </Dialog>
     </div>
