d1d123611d60b2273d233821a68b5852591aabcc
robuGdada <paudelronish@gmail.com>
2023-08-30 23:41:13 +0545

    logout dialog add

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import { Dialog, DialogContent } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { useSnapshot } from "valtio";
+import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
+import { modalStore } from "@/store/modalStore";
 
 interface Ilogout {
   onLogout: () => void;
-  onCancel: () => void;
 }
-const Logout = ({ onLogout, onCancel }: Ilogout) => {
+
+function Logout({ onLogout }: Ilogout) {
+  const themeSnap = useSnapshot(themeStore);
+  const { logout } = useSnapshot(modalStore);
+
   return (
-    <div className="logout-container">
-      <p>Do you want to logout?</p>
-      <div className="logout-btn">
-        <button className="agree-btn" onClick={onLogout}>
-          Yes
-        </button>
-        <button className="cancel-btn" onClick={onCancel}>
-          No
-        </button>
-      </div>
+    <div>
+      <Dialog open={logout.open}>
+        <DialogContent>
+          <div className="flex flex-col gap-2 mb-2">
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-4 text-center text font-bold"
+            >
+              Are you sure you want to sign out?
+            </span>
+            <div className="flex gap-12 mt-5 justify-center">
+              <Button variant={"destructive"} onClick={onLogout}>
+                Yes,sign out
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
-};
-
-export default Logout;
+}
+export { Logout };
