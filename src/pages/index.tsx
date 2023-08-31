9269f95022f06df3c26b7d39b777ba90224bb33c
robuGdada <paudelronish@gmail.com>
2023-08-31 10:20:24 +0545

    react portal for forgot pw added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Main from "@/components/Main";
 import { SignUp } from "@/components/signUp";
 import { SignIn } from "@/components/signIn";
+import ForgotPassword from "@/components/forgetPassword";
 
 const index = () => {
   return (
@@ -13,6 +14,7 @@ const index = () => {
       <Footer />
       <SignUp />
       <SignIn />
+      <ForgotPassword />
     </>
   );
 };
