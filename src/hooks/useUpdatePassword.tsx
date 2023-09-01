35479e29d6860fe3f5bff8e2be1098a26348ea20
robuGdada <paudelronish@gmail.com>
2023-09-02 00:04:58 +0545

    for updating new password

---
+++ src/hooks/useUpdatePassword.tsx
@@ -0,0 +1,19 @@
+import { API } from "@/api/API";
+import { useMutation } from "@tanstack/react-query";
+
+interface IUserData {
+  password: string;
+  token: string;
+}
+
+const updatePassword = async (newUser: IUserData) => {
+  const response = await API.post("user/update-password", newUser);
+  return response.data;
+};
+const useUpdatePassword = ({ onSuccess }: { onSuccess: any }) => {
+  return useMutation({
+    mutationFn: updatePassword,
+    onSuccess,
+  });
+};
+export { useUpdatePassword };
