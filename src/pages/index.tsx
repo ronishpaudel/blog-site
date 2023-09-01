c557a341640514e74e1bd9eea2fd1797e6a63c58
robuGdada <paudelronish@gmail.com>
2023-09-01 14:35:53 +0545

    darkmode on skeleton theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,25 @@ import Main from "@/components/Main";
 import { SignUp } from "@/components/signUp";
 import { SignIn } from "@/components/signIn";
 import ForgotPassword from "@/components/forgetPassword";
+import { SkeletonTheme } from "react-loading-skeleton";
+import { useSnapshot } from "valtio";
+import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
 
 const index = () => {
+  const themeSnap = useSnapshot(themeStore);
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
-      <SignUp />
-      <SignIn />
-      <ForgotPassword />
+      <SkeletonTheme
+        baseColor={THEME_PALETTE[themeSnap.theme].baseColor}
+        highlightColor={THEME_PALETTE[themeSnap.theme].highlightColor}
+      >
+        <Header />
+        <Main />
+        <Footer />
+        <SignUp />
+        <SignIn />
+        <ForgotPassword />
+      </SkeletonTheme>
     </>
   );
 };
