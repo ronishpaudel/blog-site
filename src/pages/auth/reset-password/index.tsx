5d30f1a72650d06eeb0ec71c14542a1bebb806d7
robuGdada <paudelronish@gmail.com>
2023-09-02 00:04:49 +0545

    reset password column add

--- src/pages/auth/reset-password/index.tsx
+++ src/pages/auth/reset-password/index.tsx
@@ -1,101 +1,108 @@
-import AuthFooter from "@/components/AuthFooter";
-import AuthHeaderlogo from "@/components/AuthHeaderlogo";
-import Button from "@/components/Button";
-import { TextInput } from "@/components/TextInput";
-import { PublicRoute } from "@/components/hoc/PublicRoute";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
+import { useSnapshot } from "valtio";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useUpdatePassword } from "@/hooks/useUpdatePassword";
+import { saveItemToLocalStorage } from "@/store/storage";
 import { useRouter } from "next/router";
-import React, { FC } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import Header from "@/components/Header";
 
-const schema = z
-  .object({
-    newPassword: z.string().min(4).max(20),
-    retypeNewPassword: z.string().min(4).max(20),
-  })
-  .refine((data) => data.newPassword === data.retypeNewPassword, {
-    message: "password dont match",
-    path: ["retypeNewPassword"],
-  });
-const index: FC = () => {
-  const { push } = useRouter();
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      newPassword: "",
-      retypeNewPassword: "",
+function index() {
+  const [password, setPassword] = useState("");
+  const [retypePassword, setRetypePassword] = useState("");
+  const { push, query } = useRouter();
+  console.log({ token: query.token });
+
+  const { mutate } = useUpdatePassword({
+    onSuccess: async (res: { token: string }) => {
+      localStorage.setItem("auth", JSON.stringify(query.token));
+      push("/");
     },
   });
+
+  function handlePasswordChange(e: any) {
+    setPassword(e.target.value);
+  }
+
+  function handleRetypePasswordChange(e: any) {
+    setRetypePassword(e.target.value);
+  }
+
+  const themeSnap = useSnapshot(themeStore);
+
+  function handleOnClick() {
+    const updatedPassword = password === retypePassword;
+
+    const token = query.token;
+    if (updatedPassword) {
+      return mutate({ password, token: String(token) });
+    } else {
+      return <div className="error-message">password not match</div>;
+    }
+  }
   return (
-    <div>
-      <AuthHeaderlogo />
+    <>
+      <Header />
       <div
-        className="pw-wrapper"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
+        className="h-[100vh] "
+        style={{ backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg }}
       >
-        <div className="inputNbtns">
-          <div className="forgetpw-text">
-            <span>Reset your password</span>
-            <p>Type in your new password</p>
-          </div>
-          <form
-            onSubmit={handleSubmit((data) => console.log(data))}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "20px",
-              maxWidth: "440px",
-              width: "100%",
-            }}
-          >
-            <div>
-              <TextInput
-                text={"New Password"}
-                placeholder={"New Password*"}
-                style={{ maxWidth: "345px", width: "100%", marginTop: "41px" }}
-                maxWidth="mW345"
-                register={register}
-                name="newPassword"
-              />
+        <div
+          style={{
+            backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+            border: `1px solid ${THEME_PALETTE[themeSnap.theme].footerBg}`,
+          }}
+          className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-md translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full"
+        >
+          <div className="reset-wrapper">
+            <div className="flex items-center justify-between  ">
+              <div>
+                <h1
+                  className="text-5xl"
+                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+                >
+                  Reset-password
+                </h1>
+              </div>
             </div>
-            <div>
-              <TextInput
-                text={"Retype New Password"}
-                placeholder={"Retype New Password*"}
-                maxWidth="mW345"
-                name="retypeNewPassword"
-                register={register}
-              />
-              {errors.retypeNewPassword?.message}
-            </div>
-            <Button
-              text={"Reset"}
-              onClick={() => push("/")}
-              style={{ marginBottom: "77px" }}
-              maxWidth="mW115"
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-10"
+            >
+              Enter your new password
+            </span>
+            <Input
+              className="max-w-md mt-2.5 border-gray-400 h-12"
+              value={password}
+              onChange={handlePasswordChange}
+              style={{
+                backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+                color: THEME_PALETTE[themeSnap.theme].textColor,
+              }}
             />
-          </form>
-          <Button
-            onClick={() => push("/auth/reset-email-password")}
-            text={"BACK TO LOGIN"}
-            preset="secondary"
-            maxWidth="mW345"
-          />
+            <span
+              style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+              className="mt-8"
+            >
+              Re-type your new password
+            </span>
+            <Input
+              className="max-w-md mt-1 border-gray-400 h-12"
+              type="password"
+              name="password"
+              value={retypePassword}
+              onChange={handleRetypePasswordChange}
+            />
+            <div className="mt-10 mb-10">
+              <Button variant={"blue"} onClick={handleOnClick}>
+                confirm new password
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <AuthFooter style={{ marginTop: "-30px" }} />
-      </div>
-    </div>
+    </>
   );
-};
-
-export default PublicRoute(index);
+}
+export default index;
