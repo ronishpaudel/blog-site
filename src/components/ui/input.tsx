7051fdda78445dfeba0157972a78a84f9c05e3d5
robuGdada <paudelronish@gmail.com>
2023-09-02 00:05:48 +0545

    chore

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,14 +1,21 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { useSnapshot } from "valtio";
+import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const themeSnap = useSnapshot(themeStore);
     return (
       <input
+        style={{
+          backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+          color: THEME_PALETTE[themeSnap.theme].textColor,
+        }}
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -17,9 +24,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
