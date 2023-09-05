150e9cf037d1b31e1e524ff0b400e3e71a70866b
robuGdada <paudelronish@gmail.com>
2023-09-05 17:51:09 +0545

    dropdown fix with value as id and name

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,80 +1,35 @@
+import React from "react";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
-import { useState } from "react";
-import { useSnapshot } from "valtio";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-function DropDown({
-  onChange,
+const Dropdown = ({
   options,
-  label,
+  onChange,
 }: {
-  onChange: (val: { id?: string; displayName: string }) => void;
+  onChange: (val: string) => void;
   options: Array<{ displayName: string; id: number }>;
   label?: string;
   style?: React.CSSProperties;
-}) {
-  const themeSnap = useSnapshot(themeStore);
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionClick = (item: { id: string; displayName: string }) => {
-    onChange(item);
-    setIsOpen(false);
-    setSelectedOption(item.displayName);
-  };
-
+}) => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-        className="h-8 w-20 rounded-sm"
-      >
-        <div
-          className="dd-header-title flex justify-center "
-          style={{
-            color: THEME_PALETTE[themeSnap.theme].textColor,
-            fontSize: "16px",
-          }}
-          onClick={toggleDropdown}
-        >
-          {selectedOption}
-        </div>
-        {selectedOption ? null : (
-          <div style={{ height: "30px" }} className="dd-list-label">
-            {label}
-          </div>
-        )}
-      </DropdownMenuTrigger>
-      {options.map((item) => (
-        <DropdownMenuContent
-          style={{
-            height: "30px",
-            color: THEME_PALETTE[themeSnap.theme].textColor,
-            backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
-          }}
-          className="dd-list-item asd mt-2 hover:to-blue-400 flex flex-col items-center"
-          key={item.id}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleOptionClick({
-              id: String(item.id),
-              displayName: item.displayName,
-            });
-          }}
-        >
-          {item.displayName}
-        </DropdownMenuContent>
-      ))}
-    </DropdownMenu>
+    <Select onValueChange={onChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {options.map((item) => (
+          <SelectItem value={JSON.stringify(item)} key={item.id}>
+            {item.displayName}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
-}
+};
 
-export { DropDown };
+export default Dropdown;
