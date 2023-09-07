edd07a7afaec43d19baef9e210cd8289a522a846
robuGdada <paudelronish@gmail.com>
2023-09-07 14:08:37 +0545

    making componennt clean

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,14 +40,9 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => {
+>(({ className, children, onCloseClick, ...props }, ref) => {
   const themeSnap = useSnapshot(themeStore);
-  function handleOnClick() {
-    modalStore.signUpModal.setOpen(false);
-    modalStore.signInModal.setOpen(false);
-    modalStore.logout.setOpen(false);
-    modalStore.forgotPassword.setOpen(false);
-  }
+
   return (
     <DialogPortal>
       <DialogOverlay />
@@ -68,7 +63,7 @@ const DialogContent = React.forwardRef<
         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
           <X
             className="h-6 w-8"
-            onClick={handleOnClick}
+            onClick={onCloseClick}
             style={{
               color: `${THEME_PALETTE[themeSnap.theme].textColor}`,
             }}
