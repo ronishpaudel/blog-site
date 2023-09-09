efd8c9838c4a0119dab6df85aef357f287b104db
robuGdada <paudelronish@gmail.com>
2023-09-09 13:53:54 +0545

    fixed search input

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,42 +1,47 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { QueryClient, dehydrate } from "@tanstack/react-query";
 import {
   Iblog,
   fetchBlogs,
   useOneBlog,
   useQueryBlog,
 } from "@/hooks/useQueryBlog";
-import { useBlogSearch } from "@/hooks/useBlogSearch";
 import { dateFormat } from "@/utils/dateFormat";
 import { Content } from "./Content";
 import Card from "./Card";
-import Ad from "./Ad";
 import { blogCreationStore } from "@/store/blogCreationStore";
 import { useSnapshot } from "valtio";
 import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
 import CardSkeleton from "./skeleton-loader/cardSkeleton";
-import MainPageSkeleton from "./skeleton-loader/mainPageSkeleton";
 
-const GG = () => {
+const BlogCardList = () => {
   const { push } = useRouter();
   const { query } = useSnapshot(blogCreationStore);
 
-  const { data: blogSearch, isLoading: searchLoading } = useBlogSearch(query);
-  const { data, isLoading } = useQueryBlog();
-
+  // const { data: blogSearch, isLoading: searchLoading } = useBlogSearch(query);
+  const {
+    data: blogSearch,
+    isLoading: searchLoading,
+    hasNextPage,
+    fetchNextPage,
+  } = useQueryBlog(query);
+  const handleViewNextPost = async () => {
+    if (hasNextPage) {
+      await fetchNextPage();
+    }
+  };
   return (
-    <div className="card-parent" id="main">
-      {searchLoading || isLoading ? <CardSkeleton amount={9} /> : ""}
-
-      {blogSearch
-        ? blogSearch.map((blog) => (
+    <>
+      <div className="card-parent mt-20" id="main">
+        {searchLoading ? <CardSkeleton amount={9} /> : ""}
+        {blogSearch?.pages.map((page: any) =>
+          page.map((blog: Iblog) => (
             <Card
               key={blog.id}
               category={blog?.category?.name}
               title={blog.title}
               thumbnailImage={blog.thumbImageUrl}
-              user={`${blog?.user?.username} `}
+              user={` ${blog?.user?.username}`}
               createdAt={dateFormat(blog.createdAt)}
               onCardClick={() => {
                 push({
@@ -49,107 +54,61 @@ const GG = () => {
               onProfileClick={() => console.log("navigate to profile with id")}
             />
           ))
-        : data?.pages.map((page: any) =>
-            page.map((blog: Iblog) => (
-              <Card
-                key={blog.id}
-                category={blog?.category?.name}
-                title={blog.title}
-                thumbnailImage={blog.thumbImageUrl}
-                user={` ${blog?.user?.username}`}
-                createdAt={dateFormat(blog.createdAt)}
-                onCardClick={() => {
-                  push({
-                    pathname: "/[id]",
-                    query: {
-                      id: blog.id,
-                    },
-                  });
-                }}
-                onProfileClick={() =>
-                  console.log("navigate to profile with id")
-                }
-              />
-            ))
-          )}
-    </div>
+        )}
+      </div>
+
+      <div className="viewPost mb-16" onClick={handleViewNextPost}>
+        View All Post
+      </div>
+    </>
   );
 };
 
 const Main = () => {
-  const {
-    hasNextPage,
-    fetchNextPage,
-    isLoading,
-    data: blogData,
-  } = useQueryBlog();
-  const handleViewNextPost = async () => {
-    if (hasNextPage) {
-      await fetchNextPage();
-    }
-  };
+  const { data: blogSearch } = useQueryBlog("");
+
+  console.log({ blogSearch });
+
   const themeSnap = useSnapshot(themeStore);
   const { push, query } = useRouter();
-  const { data } = useOneBlog(query?.id as string);
+  const { query: blogQuery } = useSnapshot(blogCreationStore);
+
+  const firstItem = blogSearch?.pages?.[0]?.[0];
   return (
     <main
       style={{
         backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg,
       }}
     >
-      {isLoading || !blogData ? (
-        <MainPageSkeleton />
+      {blogQuery ? (
+        <div className="main mt-28 mb-10 ">
+          <BlogCardList />
+        </div>
       ) : (
         <div className="main">
           <div className="image-wrappper">
             <img src="/Image.png" className="Image" alt="Blog Image" />
           </div>
-          {data ? (
-            <Content
-              title={data.title}
-              category={data.category.name}
-              createdAt={data.createdAt}
-              user={data.user.username}
-              onCardClick={() =>
-                push({
-                  pathname: "/[id]",
-                  query: {
-                    id: data.id,
-                  },
-                })
-              }
-            />
-          ) : (
-            ""
-          )}
-          <Ad />
-          <GG />
-          <div className="viewPost" onClick={handleViewNextPost}>
-            View All Post
-          </div>
-          <Ad />
+          <Content
+            title={firstItem?.title}
+            category={firstItem?.category?.name}
+            createdAt={firstItem?.createdAt}
+            user={firstItem?.user?.username}
+            onCardClick={() =>
+              push({
+                pathname: "/[id]",
+                query: {
+                  id: firstItem?.slug,
+                },
+              })
+            }
+          />
+
+          <BlogCardList />
         </div>
       )}
     </main>
   );
 };
-
-export async function getServerSideProps() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchInfiniteQuery(["blogs"], fetchBlogs, {
-    getNextPageParam: (lastPage, pages) => {
-      return lastPage.length + 1;
-    },
-    cacheTime: 5 * 60 * 1000,
-    staleTime: 4 * 60 * 1000,
-  });
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
 
 export default Main;
