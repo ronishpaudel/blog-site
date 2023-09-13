0c57be7c41168b36cb7fa6eb8f41be0fdc208988
robuGdada <paudelronish@gmail.com>
2023-09-13 15:04:48 +0545

    unused file for a while

---
+++ src/components/skeleton-loader/contentSkeleton.tsx
@@ -0,0 +1,39 @@
+import { THEME_PALETTE, themeStore } from "@/store/colorPalette.store";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import { useSnapshot } from "valtio";
+
+const ContentSkeleton = () => {
+  const themeSnap = useSnapshot(themeStore);
+
+  return (
+    <>
+      <div
+        className="content"
+        style={{
+          backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg,
+          border: `1px solid ${THEME_PALETTE[themeSnap.theme].footerBg}`,
+        }}
+        id="content"
+      >
+        <div className="heading mb-4">
+          <Skeleton width={100} height={30} />
+        </div>
+        <Skeleton count={4} />
+
+        <div className="flex gap-5 items-center mt-4">
+          <Skeleton circle width={40} height={40} className="author-img" />
+
+          <div style={{ maxWidth: "100px", width: "100%" }}>
+            <Skeleton />
+          </div>
+          <div style={{ maxWidth: "176px", width: "100%" }}>
+            <Skeleton />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { ContentSkeleton };
