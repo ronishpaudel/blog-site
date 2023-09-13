1137894af7bb4409653f923aafce6d1db9d99c35
robuGdada <paudelronish@gmail.com>
2023-09-13 13:46:38 +0545

    apply mobile responsiveness

--- src/components/skeleton-loader/cardSkeleton.tsx
+++ src/components/skeleton-loader/cardSkeleton.tsx
@@ -5,7 +5,14 @@ const CardSkeleton = ({ amount }: any) => {
   const loadCards = Array(amount).fill(1);
   return loadCards.map((card, i) => (
     <div className="card-skeleton" key={i}>
-      <div style={{ height: "250px", width: "360px", padding: "10px" }}>
+      <div
+        style={{
+          height: "250px",
+          maxWidth: "360px",
+          width: "100%",
+          padding: "10px",
+        }}
+      >
         <Skeleton style={{ height: "250px", borderRadius: "6px" }} />
       </div>
       <div className="mt-6 flex flex-col justify-center">
@@ -18,8 +25,12 @@ const CardSkeleton = ({ amount }: any) => {
         <div className="flex gap-5 items-center">
           <Skeleton circle width={40} height={40} />
 
-          <Skeleton width={100} />
-          <Skeleton width={176} />
+          <div style={{ maxWidth: "100px", width: "100%" }}>
+            <Skeleton />
+          </div>
+          <div style={{ maxWidth: "176px", width: "100%" }}>
+            <Skeleton />
+          </div>
         </div>
       </div>
     </div>
