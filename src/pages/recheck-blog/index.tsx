2f43bb048681a849c02976c03b323d8979f86cd1
robuGdada <paudelronish@gmail.com>
2023-09-15 14:41:39 +0545

    loader added

--- src/pages/recheck-blog/index.tsx
+++ src/pages/recheck-blog/index.tsx
@@ -174,7 +174,15 @@ function index() {
           </div>
 
           {isCreating || isUploading || isCreatingUpload ? (
-            ""
+            <ColorRing
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              colors={["#4b6bfb", "#4b6bfb", "#4b6bfb", "#4b6bfb", "#4b6bfb"]}
+            />
           ) : (
             <Button text={"Publish"} onClick={() => handleOnSubmit()} />
           )}
