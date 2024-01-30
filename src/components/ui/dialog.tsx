4e64e9a61c4adc6e145dee6b1a31fecc38b1eb17
robuGdada <paudelronish@gmail.com>
2024-01-30 15:19:13 +0545

    fix

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -81,7 +81,7 @@ const DialogHeader = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn("flex flex-col  text-center sm:text-left p-4", className)}
+    className={cn("flex flex-col  sm:text-left p-4", className)}
     {...props}
   />
 );
