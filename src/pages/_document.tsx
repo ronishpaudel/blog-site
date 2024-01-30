988e78e2ccb4047395553521067f4ee22e529340
robuGdada <paudelronish@gmail.com>
2024-01-30 21:09:24 +0545

    title set

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,44 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import BlogPage from "./head";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="description"
-          content="Explore insightful articles and updates on technology, business, and innovation at techEra Blog."
-        />
-        <meta
-          name="keywords"
-          content="Technology, Business, Blog, Innovation, Software Development"
-        />
-        <meta name="author" content="techEra" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>techEra Blog | Explore Tech, Business, and Innovation</title>
-
-        <meta
-          property="og:title"
-          content="techEra Blog | Explore Tech, Business, and Innovation"
-        />
-        <meta
-          property="og:description"
-          content="Explore insightful articles and updates on technology, business, and innovation at techEra Blog."
-        />
-        <meta
-          property="og:image"
-          content="URL to your blog logo or featured image"
-        />
-        <meta property="og:url" content="URL to your blog" />
-        <meta name="twitter:card" content="summary_large_image" />
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <BlogPage />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
---
+++ src/pages/head.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+function BlogPage() {
+  return (
+    <div>
+      <title>techEra Blog | Individual Blog Post Title</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta charSet="UTF-8" />
+      <meta
+        name="description"
+        content="Explore insightful articles and updates on technology, business, and innovation at techEra Blog."
+      />
+      <meta
+        name="keywords"
+        content="Technology, Business, Blog, Innovation, Software Development"
+      />
+      <meta name="author" content="techEra" />
+    </div>
+  );
+}
+
+export default BlogPage;
