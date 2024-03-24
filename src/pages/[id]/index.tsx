7f4eb5a42083036e5ea1b6462be98a6e5b356228
robuGdada <paudelronish@gmail.com>
2024-03-24 12:55:27 +0545

    unused import remove

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Author } from "@/components/Author";
