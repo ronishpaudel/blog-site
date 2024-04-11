dea41a20c2048891e34c456353b4211d788bdb37
robuGdada <paudelronish@gmail.com>
2024-04-11 20:48:02 +0545

    header ui fix and many more section add fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,7 @@ import { modalStore } from "@/store/modalStore";
 import { Logout } from "./Logout";
 import { BsSearch } from "react-icons/bs";
 import { RxCross2 } from "react-icons/rx";
+import { Input } from "./ui/input";
 
 function setThemePreference(theme: string) {
   localStorage.setItem("themePreference", theme);
@@ -61,15 +62,6 @@ const Header: FC = () => {
     }
   }
 
-  // function handleOnClick() {
-  //   if (isLightMode) {
-  //     themeStore.setTheme("dark");
-  //     setThemePreference("dark");
-  //   } else {
-  //     themeStore.setTheme("light");
-  //     setThemePreference("light");
-  //   }
-  // }
   function onClickSearchBar() {
     setShowSearchBar(true);
   }
@@ -80,69 +72,160 @@ const Header: FC = () => {
       setSearchLogo(true);
     }
   }
+
   async function handleCrossClick(e: any) {
     blogCreationStore.setQuery("");
-
     setSearchLogo(false);
   }
+
   function onPush() {
     push("/");
     setSearchLogo(false);
     blogCreationStore.setQuery("");
   }
+
   const hideItems =
     pathname !== "/create-blog" &&
     pathname !== "/recheck-blog" &&
     pathname !== "/auth/reset-password" &&
     pathname !== "/[id]" &&
     pathname !== "/myBlogs";
+
   return (
     <>
       <header
         style={{ backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg }}
       >
-        <div className="header">
-          <div className="header-logo" onClick={onPush}>
-            {isLightMode ? (
-              <img src="/techEra/techera-gray.png" alt="asd" />
-            ) : (
-              <img src="/techEra/techera-gray.png" alt="asd" />
-            )}
+        {showSearchBar ? (
+          <div className="py-4 flex gap-5 items-center max-w-[768px] w-full justify-center">
+            <div className="flex justify-center items-center relative max-w-[495px] w-full">
+              <Input
+                className="rounded-lg w-full "
+                type="text"
+                placeholder="Search"
+                value={query}
+                style={{
+                  backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+                  color: THEME_PALETTE[themeSnap.theme].textColor,
+                }}
+                onChange={handleSearch}
+              />
+              <RxCross2
+                style={{
+                  color: THEME_PALETTE[themeSnap.theme].baseColor,
+                }}
+                className="cursor-pointer text-xl absolute top-1/2 right-4 transform -translate-y-1/2 "
+                onClick={handleCrossClick}
+              />
+            </div>
+            <div
+              className="text-gray-100"
+              onClick={() => setShowSearchBar(false)}
+            >
+              Cancel
+            </div>
           </div>
+        ) : (
+          <div className="header">
+            <div className="header-logo" onClick={onPush}>
+              <img src="/techEra/techera-gray.png" alt="asd" />
+            </div>
 
-          <div className="lastchildheaderparent">
-            {hideItems && (
-              <>
-                {loggedIn ? (
+            <div className="lastchildheaderparent">
+              {hideItems && (
+                <>
+                  {loggedIn && (
+                    <div
+                      className="text-gray-500 text-[16px] hover:scale-105 cursor-pointer md:block hidden  "
+                      onClick={() => push("/myBlogs")}
+                    >
+                      MyBlogs
+                    </div>
+                  )}
                   <div
-                    className="text-gray-500 text-[16px] hover:scale-105 cursor-pointer"
-                    onClick={() => push("/myBlogs")}
+                    className="header-search"
+                    style={{
+                      backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+                    }}
                   >
-                    MyBlogs
+                    {!searchLogo ? (
+                      <img
+                        src="/search-outline.png"
+                        style={{ cursor: "pointer" }}
+                      />
+                    ) : (
+                      <RxCross2
+                        style={{
+                          color: THEME_PALETTE[themeSnap.theme].baseColor,
+                        }}
+                        className="cursor-pointer text-xl "
+                        onClick={handleCrossClick}
+                      />
+                    )}
                   </div>
-                ) : null}
+                </>
+              )}
+
+              {hideItems && (
                 <div
-                  className="header-search"
+                  className={`header-items`}
+                  onClick={() => setIsMenuOpen(false)}
                   style={{
-                    backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+                    color: THEME_PALETTE[themeSnap.theme].textColor,
                   }}
                 >
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="rounded-lg"
-                    value={query}
+                  <div
+                    className="blogs-create "
+                    onClick={handlePush}
                     style={{
                       backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
                       color: THEME_PALETTE[themeSnap.theme].textColor,
+                      border: "none",
                     }}
-                    onChange={handleSearch}
-                  />
+                  >
+                    Create
+                    <AiOutlinePlus
+                      style={{ fontSize: "15px" }}
+                      className="search-png"
+                    />
+                  </div>
 
+                  <div className="lgnin-lgnout-btn">
+                    {loggedIn ? (
+                      <>
+                        <Button
+                          onClick={handleLogoutConfirmation}
+                          className="max-w-md w-full text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer"
+                        >
+                          Logout
+                        </Button>
+                        {showLogoutConfirmation && (
+                          <Logout onLogout={handleLogout} />
+                        )}
+                      </>
+                    ) : (
+                      <Button variant={"blue"} onClick={handleLogin}>
+                        Login
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="flex items-center">
+              {showSearchBar ? (
+                <div
+                  style={{
+                    backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
+                  }}
+                  className="flex items-center rounded-lg px-[4px] w-[170px]"
+                >
                   {!searchLogo ? (
                     <img
                       src="/search-outline.png"
                       style={{ cursor: "pointer" }}
+                      className="w-[15px] h-[15px]"
                     />
                   ) : (
                     <RxCross2
@@ -154,137 +237,78 @@ const Header: FC = () => {
                     />
                   )}
                 </div>
-              </>
-            )}
-            {/* <div
-              className={`header-toggle ${
-                isLightMode ? "left-to-right" : "right-to-left"
-              }`}
-              style={!isLightMode ? { backgroundColor: "#4B6BFB" } : {}}
-              onClick={() => {
-                if (typeof window !== undefined && document) {
-                  document
-                    .querySelector(".golo")
-                    ?.classList.add(
-                      isLightMode
-                        ? "animate-move-toggle-to-right"
-                        : "animate-move-toggle-to-left"
-                    );
-                  document
-                    .querySelector(".golo")
-                    ?.classList.remove(
-                      !isLightMode
-                        ? "animate-move-toggle-to-right"
-                        : "animate-move-toggle-to-left"
-                    );
-                  setIsLightMode((prevMode) => !prevMode);
-                  handleOnClick();
-                }
-              }}
-            >
-              <div className="golo">
-                <img src="/sunny.png" />
-              </div>
-            </div> */}
-            {hideItems && (
-              <div
-                className={`header-items ${isMenuOpen ? "menu-open" : ""} `}
-                onClick={() => setIsMenuOpen(false)}
-                style={{
-                  color: THEME_PALETTE[themeSnap.theme].textColor,
-                }}
-              >
-                <div
-                  className="blogs-create "
-                  onClick={handlePush}
-                  style={{
-                    backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
-                    color: THEME_PALETTE[themeSnap.theme].textColor,
-                    border: "none",
-                  }}
-                >
-                  Create
-                  <AiOutlinePlus
-                    style={{ fontSize: "15px" }}
-                    className="search-png"
-                  />
-                </div>
-
-                <div className="lgnin-lgnout-btn">
-                  {loggedIn ? (
-                    <>
-                      <Button
-                        onClick={handleLogoutConfirmation}
-                        className="max-w-md w-full text-center text-white bg-blue-500 hover:bg-blue-600 cursor-pointer"
-                      >
-                        Logout
-                      </Button>
-                      {showLogoutConfirmation && (
-                        <Logout onLogout={handleLogout} />
-                      )}
-                    </>
-                  ) : (
-                    <Button variant={"blue"} onClick={handleLogin}>
-                      Login
-                    </Button>
+              ) : (
+                <div className="flex items-center">
+                  <div
+                    className="search-logo-btn mr-4"
+                    onClick={onClickSearchBar}
+                  >
+                    <BsSearch
+                      style={{
+                        color: THEME_PALETTE[themeSnap.theme].textColor,
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="menu-button"
+                    onClick={handleToggleMenu}
+                    style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
+                  >
+                    Menu
+                  </div>
+                  {isMenuOpen && (
+                    <div
+                      style={{
+                        backgroundColor: THEME_PALETTE[themeSnap.theme].cardBg,
+                      }}
+                      className="absolute mt-2 max-w-[768px] top-[66%] right-[1%] bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    >
+                      <div className="py-1">
+                        <a
+                          href="/myBlogs"
+                          className="block px-4 py-2 text-sm text-gray-100 hover:bg-gray-300"
+                        >
+                          My Crafted Blogs
+                        </a>
+                        <a
+                          href="/create-blog"
+                          className="block px-4 py-2 text-sm text-gray-100 hover:bg-gray-300"
+                        >
+                          Start crafting
+                        </a>
+                        <a
+                          href="https://techera.io"
+                          className="block px-4 py-2 text-sm text-gray-100 hover:bg-gray-100"
+                        >
+                          Support
+                        </a>
+                        <div
+                          className="block w-full px-4 py-2 text-left text-sm text-gray-100 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          <div>
+                            {loggedIn ? (
+                              <>
+                                <button onClick={handleLogoutConfirmation}>
+                                  Sign out
+                                </button>
+                                {showLogoutConfirmation && (
+                                  <Logout onLogout={handleLogout} />
+                                )}
+                              </>
+                            ) : (
+                              <button onClick={handleLogin}>Sign in</button>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   )}
                 </div>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex items-center">
-          {showSearchBar ? (
-            <div
-              style={{
-                backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
-              }}
-              className="flex items-center rounded-lg px-[4px] w-[170px]"
-            >
-              <input
-                type="text"
-                placeholder="Search"
-                className="rounded-lg p-[8px] w-[100%]"
-                value={query}
-                style={{
-                  backgroundColor: THEME_PALETTE[themeSnap.theme].inputBg,
-                  color: THEME_PALETTE[themeSnap.theme].textColor,
-                }}
-                onChange={handleSearch}
-              />
-              {!searchLogo ? (
-                <img
-                  src="/search-outline.png"
-                  style={{ cursor: "pointer" }}
-                  className="w-[15px] h-[15px]"
-                />
-              ) : (
-                <RxCross2
-                  style={{
-                    color: THEME_PALETTE[themeSnap.theme].baseColor,
-                  }}
-                  className="cursor-pointer text-xl "
-                  onClick={handleCrossClick}
-                />
               )}
             </div>
-          ) : (
-            <div className="flex items-center">
-              <div className="search-logo-btn mr-4" onClick={onClickSearchBar}>
-                <BsSearch
-                  style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-                />
-              </div>
-              <div
-                className="menu-button"
-                onClick={handleToggleMenu}
-                style={{ color: THEME_PALETTE[themeSnap.theme].textColor }}
-              >
-                Menu
-              </div>
-            </div>
-          )}
-        </div>
+          </div>
+        )}
       </header>
     </>
   );
