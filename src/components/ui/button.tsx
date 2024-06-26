747fe667bd3f0fa922886e0acfed4706e5495110
robuGdada <paudelronish@gmail.com>
2024-06-26 21:51:33 +0545

    chore

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -47,7 +47,6 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    const themeSnap = useSnapshot(themeStore);
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
